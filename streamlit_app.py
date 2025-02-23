99f769a5e669f8221f1884f6b1bc0b20c1aef74a
Jonas Hillebrecht <jonas.hillebrecht@googlemail.com>
2025-02-23 19:46:12 +0000

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ sys.modules['sqlite3'] = pysqlite3
 
 import streamlit as st
 from crewai import Agent, Task, Crew, Process
-from crewai_tools.tools import SerperDevTool
+from crewai_tools import SerperDevTool
 import tracemalloc
 import warnings
 import os
